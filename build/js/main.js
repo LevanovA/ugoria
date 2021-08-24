350d907d36d7b14e9b1bf87194f1364ae001ff65
Артем Леванов <tema.levanov@gmail.com>
2021-08-24 14:18:02 +0300

    adaptive for the menu

--- build/js/main.js
+++ build/js/main.js
@@ -20,8 +20,28 @@ $(document).ready(function(){
     'use strict'
 
     $('.burger').on('click', function(){
+        const menu = $('.menu')
+        const headerBlock = $('.header--main')
+        const popupActive = $('.popup--active')
+
         $(this).toggleClass('burger--active')
-        $('.menu').toggleClass('menu--active')
+        menu.toggleClass('menu--active')
+
+        // if (headerBlock.hasClass('header--active')) {
+        //     headerBlock.removeClass('header--active')
+        // }
+
+        if (menu.hasClass('menu--active')) {
+            if (popupActive.length) {
+                headerBlock.removeClass('header--active')
+            } else {
+                blockBody()
+            }
+        } else if (popupActive.length) {
+            headerBlock.addClass('header--active')
+        } else {
+            unBlockBody()
+        }
     });
 });
 
